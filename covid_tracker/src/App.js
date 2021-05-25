a80fc9f19603667699ede36a45dc44c8ca7b3214
cherindranathreddy <cherindranath@gmail.com>
2021-05-25 12:42:19 +0530

    third commit

--- covid_tracker/src/App.js
+++ covid_tracker/src/App.js
@@ -1,23 +1,120 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState,useEffect } from "react";
+import InfoBox from "./InfoBox.js";
+import Table from "./Table.js";
+import { sortData } from "./util.js";
+import { LineGraph } from "./LineGraph.js";
+
+import {
+  MenuItem,
+  FormControl,
+  Select,
+  Card,
+  CardContent,
+} from "@material-ui/core";
+
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
+  const [countryInfo,setCountryInfo] = useState({});
+  const [tableData, setTableData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/all")
+      .then((response) => response.json())
+      .then((data) => {
+        setCountryInfo(data);
+      });
+  }, []);
+
+  useEffect(()=>{
+    const getCountriesData = async () =>{
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+
+          const sortedData = sortData(data);
+          setTableData(sortedData);
+          setCountries(countries);
+        });
+      };
+
+    getCountriesData();
+
+  },[]);
+
+  const onCountryChanged = (event) => {
+    const countryCode = event.target.value;
+    
+    const url = countryCode === "worldwide"
+        ? "https://disease.sh/v3/covid-19/all"
+        : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setCountry(countryCode);
+        setCountryInfo(data);
+      })
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__left">
+        <div className="app__header">
+          <h1>COVID-19 TRACKER</h1>
+          <FormControl className="app__dropdown">
+              
+          <Select
+                  variant="outlined"
+                  onChange={onCountryChanged} value={country}
+                >
+                <MenuItem value="worldwide">Worldwide</MenuItem>
+                {countries.map((country) => (
+                  <MenuItem value={country.value}>{country.name}</MenuItem>
+                ))}
+          </Select>
+
+          </FormControl>
+        </div>
+
+
+        <div className="app__stats">
+            <InfoBox title="Coronavirus Cases" 
+             active={countryInfo.todayCases} 
+             total={countryInfo.cases}
+            />
+            <InfoBox title="Recovered"
+             active={countryInfo.todayRecovered} 
+             total={countryInfo.recovered}
+            />
+            <InfoBox title="Deaths" 
+            active={countryInfo.todayDeaths} 
+            total={countryInfo.deaths}
+           />
+        </div>
+
+      </div>
+
+
+      <Card className="app__right">
+        <CardContent>
+          <h3>Live Cases by Country</h3>
+          <Table countries={tableData} />
+        </CardContent>
+
+        <CardContent>
+        <h3>WorldWide new cases</h3>
+          <LineGraph />
+        </CardContent>
+
+      </Card>
     </div>
   );
 }
---
+++ covid_tracker/src/InfoBox.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Card,CardContent,Typography} from "@material-ui/core";
+
+function InfoBox({title,active,total}) {
+    return (
+        <Card>
+            <CardContent>
+                <Typography className="infoBox__title" color="textSecondary">
+                    {title}
+                </Typography>
+
+                <h2 className= "infoBox__cases">
+                    {active}
+                </h2>
+
+                <Typography className="infoBox__total" color="textSecondary">
+                {total} Total
+                </Typography>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default InfoBox;
